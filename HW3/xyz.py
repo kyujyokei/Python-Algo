e3579f16f032a4d238334b1b4e506bb41294b024
kyujyokei <kyujyokei@gmail.com>
2018-01-28 20:37:10 -0800

    HW3 Completed

--- HW3/xyz.py
+++ HW3/xyz.py
@@ -1,11 +1,32 @@
 def find(a):
     list = []
-    left = 0
-    right = len(a) - 1
-    print(left, right)
+    a.sort()
+    # print(a)
 
+    # print(left, right)
+    for i in a:
+        left = 0
+        right = len(a) - 1
+        while left < right:
+            # print("L: ",a[left]," ,R: ",a[right]," ,i: ", i)
+            if a[left] + a[right] < i:
+                # print("BIG",a[left],a[right],a[i])
+                left += 1
+                # print("LEFT +\n")
+            elif a[left] + a[right] > i:
+                # print("SMALL",a[left], a[right], a[i])
+                right -= 1
+                # print("RIGHT -\n")
 
-find([1, 4, 2, 3, 5])
+            else:
+                # print("EQUAL", a[left], a[right], i)
+                list.append([a[left], a[right], i])
+                left += 1
+                right -= 1
+
+    return list
+
+print(find([1, 4, 2, 3, 5]))
 # returns[(1, 3, 4), (1, 2, 3), (1, 4, 5), (2, 3, 5)]
 #
 # Filename: xyz.py
