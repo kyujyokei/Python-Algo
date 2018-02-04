ef67d4104ee5a40d1b4cb0b1241d39ee0995c71d
Kejo\Hsieh <kyujyokei@gmail.com>
2018-02-03 23:42:30 -0800

    Half way through HW4

---
+++ HW4/kmergesort.py
@@ -0,0 +1,10 @@
+
+
+# 2. k-way mergesort (the classical mergesort is a special case where k=2).
+#
+#    >>> kmergesort([4,1,5,2,6,3,7,0], 3)
+#    [0,1,2,3,4,5,6,7]
+#
+#    Q: What is the complexity? Write down the detailed analysis in report.txt.
+#
+#    Filename: kmergesort.py
