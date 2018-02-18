38e5702f9df681967dfcd16b0bee465e44731413
kyujyokei <kyujyokei@gmail.com>
2018-02-18 14:29:10 -0800

    Trying to implement solution(), but giving wrong answers now

--- HW6/knapsack_bounded.py
+++ HW6/knapsack_bounded.py
@@ -25,7 +25,7 @@
 def best(x, items):
     n = len(items)
     items.insert(0, (0, 0, 0))
-    opt = [[[0,[0 for a in range(0,len(items)-1)]] for i in range(0,x+1)] for j in range(0,n+1)]
+    opt = [[[0,0] for i in range(0,x+1)] for j in range(0,n+1)]
 
     for i in range(1, n+1):
         weight, value, copies = items[i][0],items[i][1], items[i][2]
@@ -36,13 +36,24 @@ def best(x, items):
                 if k*weight <= j:
                     max_c = min(k,copies)
                     list[i-1] = max_c
-                    last_list = opt[i-1][j-max_c*weight][1]
-                    res_list = [x + y for x, y in zip(list, last_list)]
-                    temp += [[opt[i-1][j-max_c*weight][0]+max_c*value,res_list]]
+                    temp += [[opt[i-1][j-max_c*weight][0]+max_c*value,max_c]]
 
             opt[i][j] = max(temp)
+    def solution(sum, num):
+        temp_x = x
+        n = len(items) -1
+        res = [0 for a in range(len(items)-1)]
+        for i in range(n-1,-1,-1):
+            print(opt[i+1][temp_x])
+            res[i] = opt[i+1][temp_x][1]
+            temp_x -= items[i][0]*res[i]
 
-    return opt[i][j][0],opt[i][j][1]
+        return res
+
+    print(opt)
+    res = solution(opt[i][j][0],opt[i][j][1])
+    # print(opt)
+    return opt[i][j][0],res
 
 
 #    Bounded Knapsack
@@ -51,14 +62,14 @@ def best(x, items):
 #    **All numbers are positive integers.**
 #    What's the best value for a bag of W?
 #
-# print(best(6, [(2, 4, 2), (3, 5, 10)]))
+# print(best(5, [(2, 4, 2), (3, 5, 10)]))
 #    (5, [0, 1])
 #
 #    the input to the best() function is W and a list of triples (w_i, v_i, c_i).
 #
 #    tie-breaking: same as in p1:
 #
-# print(best(3, [(1, 5, 2), (1, 5, 3)]))
+print(best(3, [(1, 5, 2), (1, 5, 3)]))
 #    (15, [2, 1])
 #
 # print(best(3, [(1, 5, 1), (1, 5, 3)]))
@@ -67,7 +78,7 @@ def best(x, items):
 # print(best(20, [(1, 10, 6), (3, 15, 4), (2, 10, 3)]))
 #    (130, [6, 4, 1])
 #
-print(best(92, [(1, 6, 6), (6, 15, 7), (8, 9, 8), (2, 4, 7), (2, 20, 2)]))
+# print(best(92, [(1, 6, 6), (6, 15, 7), (8, 9, 8), (2, 4, 7), (2, 20, 2)]))
 #    (236, [6, 7, 3, 7, 2])
 #
 #    Q: What are the time and space complexities?
