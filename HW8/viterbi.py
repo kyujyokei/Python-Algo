76095ab268d246c430765b0fb674834677057693
kyujyokei <kyujyokei@gmail.com>
2018-03-01 20:27:46 -0800

    Half way into viterbi

--- HW8/viterbi.py
+++ HW8/viterbi.py
@@ -1,3 +1,56 @@
+def longest(n, list):
+    order, ad_list = topol(n, list)
+    paths = {}
+
+    if order == None: return None
+    # print(ad_list)
+    for i in order:
+        if i in ad_list:
+            paths.setdefault(i, []).append([0, [i]])
+            for x in ad_list[i]:
+                # print(paths[i][0][1])
+                print(i, paths[i])
+                last_sum, last_path = paths[i][0]
+                paths[x] = [last_sum + 1, last_path + [x]]
+
+
+
+        print(paths, i)
+    return 0
+
+
+
+
+def topol (n, list):
+    ad_list = dict()
+    indg = dict()
+    queue = []
+    res = []
+
+    def map (list):
+        for x in list:
+            start, end = x
+            if start not in indg: indg[start] = 0
+            if end not in indg: indg[end] = 1
+            else: indg[end] = indg[end] + 1
+            ad_list.setdefault(start, []).append(end)
+
+    map(list)
+
+    for i in indg:
+        if indg[i] == 0: queue.append(i)
+
+    while queue:
+
+        node = queue.pop(0)
+        res += [node]
+        if node in ad_list:
+            for i in ad_list[node]:
+                indg[i] -= 1
+                if indg[i] == 0: queue.append(i)
+    return res, ad_list if len(res) == n else None
+
+
 # 3. [WILL BE GRADED]
 #    Viterbi Algorithm For Longest Path in DAG (see DPV 4.7, [2], CLRS problem 15-1)
 #
@@ -13,7 +66,7 @@
 #
 #    e.g., for the above example:
 #
-#    >>> longest(8, [(0,2), (1,2), (2,3), (2,4), (3,4), (3,5), (4,5), (5,6), (5,7)])
+longest(8, [(0,2), (1,2), (2,3), (2,4), (4,3), (3,5), (4,5), (5,6), (5,7)])
 #    (5, [0, 2, 3, 4, 5, 6])
 #
 #    Tie-breaking: arbitrary. any longest path is fine.
