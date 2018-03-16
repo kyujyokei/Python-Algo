1c47cc722d31e4700bd2efac19f0688669489d39
kyujyokei <kyujyokei@gmail.com>
2018-03-15 22:39:23 -0700

    Half way through re-writing q1 with method 2

--- HW10/rna.py
+++ HW10/rna.py
@@ -41,25 +41,6 @@ def best(n):
 
     d = {'': [0, ''], 'A': [0, '.'], 'U': [0, '.'], 'C': [0, '.'], 'G': [0, '.']}
 
-    # def _find(n, i, j):
-    #     # print(n[i:j])
-    #     temp = []
-    #     for k in range(i+1, j+1):
-    #         # print(n[i:k], n[k:j])
-    #         if n[i:k] not in d:
-    #             d[n[i:k]] = find(n, i, k-1)
-    #
-    # 
-    #         if n[k:j] not in d:
-    #             # if n[k:j] == 'AC':
-    #             #     print("XXX")
-    #             d[n[k:j]] = find(n, k, j)
-    #
-    #         if len (d[n[i:k]][1] + d[n[k:j]][1]) == len(n[i:j]):
-    #             # print(n[i:k], n[k:j],[d[n[i:k]][0] , d[n[k:j]][0], d[n[i:k]][1] , d[n[k:j]][1]])
-    #             temp.append([d[n[i:k]][0] + d[n[k:j]][0], d[n[i:k]][1] + d[n[k:j]][1]])
-    #         # print("TEMP:", temp)
-    #     return max(temp, key= lambda x: x[0])
 
     def pair(a):
         rna = set(['AU', 'GC', 'GU', 'UA', 'CG', 'UG'])
@@ -67,20 +48,26 @@ def best(n):
 
     def find(n,i,j):
 
-
         print(n[i:j])
         if n[i:j] in d:
             return d[n[i:j]]
-        temp = []
+
+        matched = []
         for k in range(i+1, j):
+            print(n[i] + n[k])
             if pair(n[i] + n[k]):  # if they make a pair
-                sum, path = find(n, i + 1, k - 1)
-                d[n[i:k]] = [sum + 1, '(' + path + ')']
-                d[n[k:j]] = find(n, k+1, j)
-                temp.append()
+                print(k, i, j)
+                sum, path = find(n, i+1, k-1)
+                # d[n[i:k]] = [sum + 1, '(' + path + ')']
+                sum1, path1 = find(n, k+1, j)
+                # d[n[k:j]] = [sum1, path1]
+                print(sum, path, sum1, path1)
+                matched.append([sum+sum1,path+path1])
+            print("MATCHED:",matched)
+
         # d[n[i:j]] = max(find(n,i,j),find(n,i+1,j), key= lambda x: x[0])
 
-        return 0
+        return d[n[i:j]]
     # find(n,i,j)
 
 
@@ -93,5 +80,4 @@ def total():
 def kbest():
     return 0
 
-
-print(best('UUCAGGA'))
+print(best('GCACG'))
