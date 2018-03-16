99b33b0576bcb051f5f3726f6e2019a0ac2a1440
kyujyokei <kyujyokei@gmail.com>
2018-03-15 21:44:12 -0700

    Commit before cleaning piles of buggy mess

--- HW10/rna.py
+++ HW10/rna.py
@@ -1,8 +1,8 @@
 
-def best(n):
+def best1(n):
     d = {'': [0, ''], 'A': [0, '.'], 'U': [0, '.'], 'C': [0, '.'], 'G': [0, '.'], 'AU': [1, '()'], 'GC': [1, '()'],
          'GU': [1, '()'], 'UA': [1, '()'], 'CG': [1, '()'], 'UG': [1, '()'], 'AC': [0, '..'], 'CA': [0, '..'],
-         'UC': [0, '..'], 'CU': [0, '..'], 'AG': [0, '..'], 'GA': [0, '..']}
+         'UC': [0, '..'], 'CU': [0, '..'], 'AG': [0, '..'], 'GA': [0, '..'], 'AA': [0, '..'], 'GG': [0, '..']}
 
     def pair(a):
         rna = set(['AU', 'GC', 'GU', 'UA', 'CG', 'UG'])
@@ -34,6 +34,58 @@ def best(n):
     return tuple(find(n,0,len(n)))
 
 
+def best(n):
+
+    i, j = 0, len(n)
+
+
+    d = {'': [0, ''], 'A': [0, '.'], 'U': [0, '.'], 'C': [0, '.'], 'G': [0, '.']}
+
+    # def _find(n, i, j):
+    #     # print(n[i:j])
+    #     temp = []
+    #     for k in range(i+1, j+1):
+    #         # print(n[i:k], n[k:j])
+    #         if n[i:k] not in d:
+    #             d[n[i:k]] = find(n, i, k-1)
+    #
+    # 
+    #         if n[k:j] not in d:
+    #             # if n[k:j] == 'AC':
+    #             #     print("XXX")
+    #             d[n[k:j]] = find(n, k, j)
+    #
+    #         if len (d[n[i:k]][1] + d[n[k:j]][1]) == len(n[i:j]):
+    #             # print(n[i:k], n[k:j],[d[n[i:k]][0] , d[n[k:j]][0], d[n[i:k]][1] , d[n[k:j]][1]])
+    #             temp.append([d[n[i:k]][0] + d[n[k:j]][0], d[n[i:k]][1] + d[n[k:j]][1]])
+    #         # print("TEMP:", temp)
+    #     return max(temp, key= lambda x: x[0])
+
+    def pair(a):
+        rna = set(['AU', 'GC', 'GU', 'UA', 'CG', 'UG'])
+        return a in rna
+
+    def find(n,i,j):
+
+
+        print(n[i:j])
+        if n[i:j] in d:
+            return d[n[i:j]]
+        temp = []
+        for k in range(i+1, j):
+            if pair(n[i] + n[k]):  # if they make a pair
+                sum, path = find(n, i + 1, k - 1)
+                d[n[i:k]] = [sum + 1, '(' + path + ')']
+                d[n[k:j]] = find(n, k+1, j)
+                temp.append()
+        # d[n[i:j]] = max(find(n,i,j),find(n,i+1,j), key= lambda x: x[0])
+
+        return 0
+    # find(n,i,j)
+
+
+    return find(n,i,j), d
+
 
 def total():
     return 0
@@ -42,4 +94,4 @@ def kbest():
     return 0
 
 
-# print(best('AGGCAUCAAACCCUGCAUGGGAGCACCGCCACUGGCGAUUUUGGUA'))
+print(best('UUCAGGA'))
