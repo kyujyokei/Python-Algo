6e9361939523fad51481279b4783762a7fd5ce17
kyujyokei <kyujyokei@gmail.com>
2018-03-15 17:06:36 -0700

    Half way into correcting code with TA's advice

--- HW10/rna.py
+++ HW10/rna.py
@@ -1,37 +1,36 @@
 
 def best(n):
 
-    d = {'':[0,''],'A':[0,'.'], 'U':[0,'.'], 'C':[0,'.'], 'G':[0,'.'], 'AU':[1,'()'], 'GC':[1,'()'], 'GU':[1,'()'], 'UA':[1,'()'], 'CG':[1,'()'], 'UG':[1,'()'], 'AC':[0,'..'], 'CA':[0,'..'], 'UC':[0,'..'], 'CU':[0,'..'], 'AG':[0,'..'], 'GA':[0,'..']}
+    d = {'':[0,''],'A':[0,'.'], 'U':[0,'.'], 'C':[0,'.'], 'G':[0,'.'], 'AU':[1,'()'], 'GC':[1,'()'], 'GU':[1,'()'], 'UA':[1,'()'], 'CG':[1,'()'], 'UG':[1,'()'], ''}
 
     def pair(a):
         rna = set(['AU', 'GC', 'GU', 'UA', 'CG', 'UG'])
         return a in rna
 
-    def find(n):
-
-        i, j = 0, len(n)
+    def find(n, i, j):
         if n in d:
             return d[n]
         else:
             if pair(n[i] + n[j-1]): # if they make a pair
-                # print(n[i] + n[j-1])
-                sum, path = find(n[i+1:j-1])
-                d[n] = [sum + 1, '(' + path + ')']
-                return d[n]
+
+                sum, path = find(n,i+1,j-1)
+                # print(n,n[i] + n[j-1], sum, path)
+                d[n[i:j]] = [sum + 1, '(' + path + ')']
+                return d[n[i:j]]
             else:
                 temp = []
-                for k in range(1,j):
-                    if n[0:k] not in d:
-                        d[n[0:k]] = find(n[0:k])
+                for k in range(i+1,j):
+                    if n[i:k] not in d:
+                        d[n[i:k]] = find(n,i,k)
                     if n[k:j] not in d:
-                        d[n[k:j]] = find(n[k:j])
-                    print("D:",d[n[0:k]], d[n[k:j]])
-                    temp.append([d[n[0:k]][0]+d[n[k:j]][0], d[n[0:k]][1]+d[n[k:j]][1]])
-                # print(temp)
+                        d[n[k:j]] = find(n,k,j)
+
+                    temp.append([d[n[i:k]][0]+d[n[k:j]][0], d[n[i:k]][1]+d[n[k:j]][1]])
+                print(i,j,temp)
                 return max(temp, key= lambda x: x[0])
+    return tuple(find(n,0,len(n)))
 
 
-    return find(n)
 
 def total():
     return 0
@@ -40,4 +39,4 @@ def kbest():
     return 0
 
 
-print(best('AGGCAUCAAACCCUGCAUGGGAGCACCGCCACUGGCGAUUUUGGUA'))
+print(best('UUCAGGA'))
