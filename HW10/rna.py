eebd88f8b15f38ec459a8740876bffa75136369e
kyujyokei <kyujyokei@gmail.com>
2018-03-15 17:19:57 -0700

    Fixed with TA's suggestions, time improved a bit, but still 1 error & 2 time limit exceed

--- HW10/rna.py
+++ HW10/rna.py
@@ -1,32 +1,35 @@
 
 def best(n):
-
-    d = {'':[0,''],'A':[0,'.'], 'U':[0,'.'], 'C':[0,'.'], 'G':[0,'.'], 'AU':[1,'()'], 'GC':[1,'()'], 'GU':[1,'()'], 'UA':[1,'()'], 'CG':[1,'()'], 'UG':[1,'()'], ''}
+    d = {'': [0, ''], 'A': [0, '.'], 'U': [0, '.'], 'C': [0, '.'], 'G': [0, '.'], 'AU': [1, '()'], 'GC': [1, '()'],
+         'GU': [1, '()'], 'UA': [1, '()'], 'CG': [1, '()'], 'UG': [1, '()'], 'AC': [0, '..'], 'CA': [0, '..'],
+         'UC': [0, '..'], 'CU': [0, '..'], 'AG': [0, '..'], 'GA': [0, '..']}
 
     def pair(a):
         rna = set(['AU', 'GC', 'GU', 'UA', 'CG', 'UG'])
         return a in rna
 
     def find(n, i, j):
-        if n in d:
-            return d[n]
+        print(i,j,n[i:j])
+        if n[i:j] in d:
+            return d[n[i:j]]
         else:
             if pair(n[i] + n[j-1]): # if they make a pair
 
                 sum, path = find(n,i+1,j-1)
-                # print(n,n[i] + n[j-1], sum, path)
+                print(n,n[i] + n[j-1], sum, path)
                 d[n[i:j]] = [sum + 1, '(' + path + ')']
                 return d[n[i:j]]
             else:
                 temp = []
                 for k in range(i+1,j):
+                    print(n[i+1:j],k)
                     if n[i:k] not in d:
                         d[n[i:k]] = find(n,i,k)
                     if n[k:j] not in d:
                         d[n[k:j]] = find(n,k,j)
 
                     temp.append([d[n[i:k]][0]+d[n[k:j]][0], d[n[i:k]][1]+d[n[k:j]][1]])
-                print(i,j,temp)
+
                 return max(temp, key= lambda x: x[0])
     return tuple(find(n,0,len(n)))
 
@@ -39,4 +42,4 @@ def kbest():
     return 0
 
 
-print(best('UUCAGGA'))
+# print(best('AGGCAUCAAACCCUGCAUGGGAGCACCGCCACUGGCGAUUUUGGUA'))
