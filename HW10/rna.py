2f4bc21aa94375ab5abb1ff8baa5c9012ea8ea1e
kyujyokei <kyujyokei@gmail.com>
2018-03-18 21:23:43 -0700

    Think is workinggit add -Agit add -A

--- HW10/rna.py
+++ HW10/rna.py
@@ -95,44 +95,64 @@ def kbest(n, k):
                         if [[res1[a][0] + res2[b+1][0] - 1, '(' + res1[a][1] + ')' + res2[b+1][1]], a, b+1] not in heap:
                             heappush(heap, [res1[a][0] + res2[b+1][0] - 1, '(' + res1[a][1] + ')' + res2[b+1][1], a, b+1])
             if len(matched) == 0 : matched = [[0,'.' * len(n[i:j])]]
-            matrix.append(matched)
+            matrix.append([matched])
 
             # print("Matrix: ", matrix, n[i:j])
 
+        nex = [find(i+1, j)]
 
-        # for those whose i is not a (
-        nex = find(i+1, j)
+        # res = [matched[0]+[0, 0], [nex[0][0],'.' + nex[0][1],1,0]]
+        # print("MATRIX: ", matrix)
+        heap = [[matrix[i][0][0][0], matrix[i][0][0][1], i, 0, 0] for i, a in enumerate(matrix)]
+        last = len(matrix)
 
+        heap.append([nex[0][0][0],'.' + nex[0][0][1], last, 0, 0])
 
-        res = [matched[0]+[0, 0], [nex[0][0],'.' + nex[0][1],1,0]]
-        # print(res)
-        heapify(res)
+        # print("NEX:", nex, ", LAST: ", last)
+        # print("HEAP:",heap)
+        # print()
+        heapify(heap)
         temp = []
-        # print("RES:",res)
+
         for _ in range(0,k):
             # print("H-POP R:", res[0], k, i, j)
-            if not res: break
-            [summ, path, id, num] = heappop(res)
+            if not heap: break
+            [summ, path, m_id, a, b] = heappop(heap)
+            # print("Popped Now:", [summ, path, m_id, a, b])
+            # print("MatrixPopped:", matrix[m_id])
+
+
             if [summ, path] not in temp:
                 temp.append([summ, path])
-            if id == 0 and num+1 < len(matched): # popped element come from matched
+            if m_id < last: # comes from matched pairs
 
-                m_sum, m_path = heappop(matched)
+                if a + 1 < len(matrix[m_id]):
+                    heappush(heap, [matrix[m_id][a+1][b][0], matrix[m_id][a+1][b][1], m_id, a+1, b])
+                    # print("A:",heap)
+                if b + 1 < len(matrix[m_id][a]):
+                    heappush(heap, [matrix[m_id][a][b+1][0], matrix[m_id][a][b+1][1], m_id, a, b+1])
+                    # print("B:", [matrix[m_id][a][b+1][0], matrix[m_id][a][b+1][1], m_id, a, b+1])
+            else: # comes from nex
+                if a + 1 < len(nex):
+                    heappush(heap, [nex[a + 1][b][0], '.' + nex[a + 1][b][1], last, a + 1, b])
+                    # print("LAST A:", heap)
+                if b + 1 < len(nex[a]):
+                    heappush(heap, [nex[a][b+1][0], '.' + nex[a][b+1][1], last, a, b+1])
+                    # print("LAST B:", [nex[a][b+1][0], '.' + nex[a][b+1][1], last, a, b+1])
 
-                heappush(res,[m_sum, m_path, 0, num+1])
-
-            elif id == 1 and num+1 < len(nex): # popped element come from next
-
-                n_sum, n_path = nex[num+1]
-
-                heappush(res, [n_sum, '.' + n_path,1, num+1])
+            # print()
 
             # if len(res) >= k: break
         # next_com = [next_sum, '.' + str(next_path)]
+
         d[n[i:j]] = temp
 
         return d[n[i:j]]
 
-    return find(i, j)
+    f = find(i, j)
 
-print(kbest('GUAC',10))
+    f = [ [i*-1, _] for i, _ in f ]
+
+    return f
+
+# print(kbest('CCCGGG',10))
